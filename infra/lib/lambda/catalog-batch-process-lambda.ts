d60a63f760f833747ab459953e238e77042ca765
Illia Miroshnychenko <illia_miroshnychenko@epam.com>
2024-11-12 18:26:13 +0200

    Task 6. Improvements

--- infra/lib/lambda/catalog-batch-process-lambda.ts
+++ infra/lib/lambda/catalog-batch-process-lambda.ts
@@ -1,50 +1,65 @@
 import { v4 as uuidv4 } from "uuid";
 import { SQSHandler } from "aws-lambda";
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
 import { productBodySchema } from "../../shared/productSchema";
+import {
+  DynamoDBDocumentClient,
+  TransactWriteCommand,
+  TransactWriteCommandInput,
+} from "@aws-sdk/lib-dynamodb";
 
 const dynamoDbClient = new DynamoDBClient({ region: "us-east-1" });
+const docClient = DynamoDBDocumentClient.from(dynamoDbClient);
 const snsClient = new SNSClient({ region: "us-east-1" });
 
 const tableName = process.env.TABLE_NAME;
 const snsTopicArn = process.env.SNS_TOPIC_ARN;
 
 export const handler: SQSHandler = async (event) => {
-  try {
-    for (const record of event.Records) {
-      const product: typeof productBodySchema = record.body
-        ? JSON.parse(record.body)
-        : {};
+  const transactionItems = event.Records.flatMap((record) => {
+    const product: typeof productBodySchema = record.body
+      ? JSON.parse(record.body)
+      : {};
 
-      const parsedProduct = productBodySchema.parse(product);
+    const parsedProduct = productBodySchema.parse(product);
 
-      const params = {
-        TableName: tableName,
-        Item: {
-          id: { S: uuidv4() },
-          createdAt: { N: new Date().getTime().toFixed() },
-          count: { N: parsedProduct.count.toString() },
-          price: { N: parsedProduct.price.toString() },
-          title: { S: parsedProduct.title },
-          description: { S: parsedProduct.description },
+    const productItem = {
+      id: { S: uuidv4() },
+      createdAt: { N: new Date().getTime().toFixed() },
+      count: { N: parsedProduct.count.toString() },
+      price: { N: parsedProduct.price.toString() },
+      title: { S: parsedProduct.title },
+      description: { S: parsedProduct.description },
+    };
+
+    return [
+      {
+        Put: {
+          TableName: tableName,
+          Item: productItem,
         },
-      };
+      },
+    ];
+  });
 
-      const command = new PutItemCommand(params);
-      await dynamoDbClient.send(command);
+  const transactionParams: TransactWriteCommandInput = {
+    TransactItems: transactionItems,
+  };
 
-      const snsMessage = {
-        subject: "New Product Created",
-        message: `A new product has been created: ${JSON.stringify(product)}`,
-      };
-      const publishCommand = new PublishCommand({
-        TopicArn: snsTopicArn,
-        Message: snsMessage.message,
-        Subject: snsMessage.subject,
-      });
-      await snsClient.send(publishCommand);
-    }
+  try {
+    await docClient.send(new TransactWriteCommand(transactionParams));
+
+    const snsMessage = {
+      subject: "Products Created",
+      message: `Products saved successfully`,
+    };
+    const publishCommand = new PublishCommand({
+      TopicArn: snsTopicArn,
+      Message: snsMessage.message,
+      Subject: snsMessage.subject,
+    });
+    await snsClient.send(publishCommand);
   } catch (error) {
     console.error("Error adding products to DynamoDB:", error);
   }
